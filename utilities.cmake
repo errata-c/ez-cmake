0bb5059285d2122b28e63373c1685b4d66d4ffb7
errata-c <erratachem@gmail.com>
2021-11-17 18:11:01 -0500

    Fixed bug with macro in utilities

--- utilities.cmake
+++ utilities.cmake
@@ -1,7 +1,11 @@
 
 macro(ez_is_project_top_level EZ_RESULT)
 	get_directory_property(EZ_HAS_PARENT PARENT_DIRECTORY)
-	set(${EZ_RESULT} ${EZ_HAS_PARENT} PARENT_SCOPE)
+	if(EZ_HAS_PARENT)
+		set(${EZ_RESULT} TRUE)
+	else()
+		set(${EZ_RESULT} FALSE)
+	endif()
 	unset(EZ_HAS_PARENT)
 endmacro()
 
