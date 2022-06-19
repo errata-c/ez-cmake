ab9e04bfaeec848277a927cb2037c8b48c682081
errata-c <erratachem@gmail.com>
2022-06-19 04:02:41 -0400

    Added a macro to simplify finding target dependency

--- utilities.cmake
+++ utilities.cmake
@@ -1,4 +1,4 @@
-
+# Sets the result variable to TRUE if the project is top level, false otherwise.
 macro(ez_is_project_top_level EZ_RESULT)
 	get_directory_property(EZ_HAS_PARENT PARENT_DIRECTORY)
 	if(EZ_HAS_PARENT)
@@ -9,3 +9,9 @@ macro(ez_is_project_top_level EZ_RESULT)
 	unset(EZ_HAS_PARENT)
 endmacro()
 
+# Find a package dependency if a target doesn't exist.
+macro(ez_find_target_dependency EZ_TARGET_NAME EZ_PACKAGE_NAME)
+	if(NOT TARGET EZ_TARGET_NAME)
+		find_dependency(${EZ_PACKAGE_NAME} ${ARGN})
+	endif()
+endmacro()
