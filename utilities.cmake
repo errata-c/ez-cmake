48546fcd20b84a23d8c02d3c6d7152a9f4f46548
errata-c <erratachem@gmail.com>
2022-06-19 04:05:03 -0400

    Moved the utility macro to the config files

--- utilities.cmake
+++ utilities.cmake
@@ -9,9 +9,3 @@ macro(ez_is_project_top_level EZ_RESULT)
 	unset(EZ_HAS_PARENT)
 endmacro()
 
-# Find a package dependency if a target doesn't exist.
-macro(ez_find_target_dependency EZ_TARGET_NAME EZ_PACKAGE_NAME)
-	if(NOT TARGET EZ_TARGET_NAME)
-		find_dependency(${EZ_PACKAGE_NAME} ${ARGN})
-	endif()
-endmacro()
